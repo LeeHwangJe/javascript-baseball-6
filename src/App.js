75500e4afa3266fb54abaddce70ffa54b184cf09
이황제 <happy_kaiser@hotmail.com>
2023-10-25 03:34:49 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { getComputer } from "./getComputer";
+import { getRestart } from "./getRestart";
+import { getUser } from "./getUser";
+import { getResult } from "./getResult";
 
 class App {
-  async play() {}
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    let computer = getComputer();
+
+    while (true) {
+      const USER = await getUser();
+      let correct = getResult(computer, USER);
+
+      if (correct) {
+        const RESTART = await getRestart();
+
+        if (RESTART === 1) {
+          computer = getComputer();
+        } else {
+          break;
+        }
+      }
+    }
+  }
 }
 
 export default App;
--- src/getRestart.js
+++ src/getRestart.js
@@ -1,10 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
 
-const reStart = async () => {
+const restartHandler = async () => {
   let restart = 0;
 
-  Console.print("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
-  Console.print("게임을 새로 시작하려면 1, 종료하려면 2을 입력하세요");
+  Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
   const INPUT = await Console.readLineAsync("");
   restart = parseInt(INPUT);
@@ -15,4 +15,4 @@ const reStart = async () => {
   return restart;
 };
 
-export { reStart };
+export { restartHandler };
